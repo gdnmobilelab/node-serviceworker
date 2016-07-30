a1e9f4ad797a73b37fedfe61ac4ed10df0a06bb0
Gabriel Wicke <gwicke@wikimedia.org>
2016-07-29 22:01:48 -0700

    Implement ServiceWorkerContainer.getRegistration()

--- test/index.js
+++ test/index.js
@@ -3,15 +3,14 @@ var ServiceWorkerContainer = require('../lib/');
 module.exports = {
     Install: {
         "Basic install & fetch": function() {
-            return (new ServiceWorkerContainer())
+            const testURL = 'https://en.wikipedia.org/w/iki/Foobar';
+            const container = new ServiceWorkerContainer();
+            return container
                 .register('https://en.wikipedia.org/test/sw.js', { scope: '/w/iki/' })
-                .then(function(registration) {
-                    return registration.fetch('https://en.wikipedia.org/w/iki/Foobar');
-                })
-                .then(function(res) {
-                    return res.text();
-                })
-                .then(function(txt) {
+                .then(() => container.getRegistration(testURL))
+                .then(registration => registration.fetch(testURL))
+                .then(res => res.text())
+                .then(txt => {
                     if (!/FOOBAR/.test(txt)) {
                         throw new Error('Expected FOOBAR in result HTML!');
                     };
